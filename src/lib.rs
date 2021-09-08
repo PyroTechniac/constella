c575e7b80b2ef750109c540912ffa0cb3d593d37
Gryffon Bellish <owenbellish@gmail.com>
2021-09-07 21:24:49 -0400

    fix: fixed what i needed to

--- src/lib.rs
+++ src/lib.rs
@@ -24,14 +24,7 @@ pub struct DataHolder<V, T> {
 	_marker: PhantomData<T>,
 }
 
-impl<V, T> DataHolder<V, T> {
-	pub const fn new(data: V) -> Self {
-		Self {
-			inner: data,
-			_marker: PhantomData,
-		}
-	}
-}
+pub type DataTransformer<V> = DataHolder<<V as Transformer>::DataType, V>;
 
 impl<V, T> DataHolder<V, T>
 where
@@ -46,6 +39,27 @@ where
 
 	pub fn into_value(&self) -> T {
 		T::revert(&self.inner)
+	}
+}
+
+impl<V, T> PersistentEmbedded for DataHolder<V, T>
+where
+	V: PersistentEmbedded,
+	T: Transformer<DataType = V>,
+{
+	fn read(read: &mut dyn Read) -> SRes<Self>
+	where
+			Self: Sized {
+		let inner = V::read(read)?;
+
+		Ok(Self {
+			inner,
+			_marker: PhantomData
+		})
+	}
+
+	fn write(&self, write: &mut dyn Write) -> SRes<()> {
+		self.inner.write(write)
 	}
 }
 
@@ -88,25 +102,50 @@ impl<V: Clone, T> Clone for DataHolder<V, T> {
 
 impl<V: Copy, T> Copy for DataHolder<V, T> {}
 
-impl<V: PartialEq, T> PartialEq for DataHolder<V, T> {
-	fn eq(&self, other: &Self) -> bool {
-		self.inner.eq(&other.inner)
+impl<V: PartialEq, T> PartialEq<V> for DataHolder<V, T> {
+	fn eq(&self, other: &V) -> bool {
+		self.inner.eq(other)
 	}
 }
 
-impl<V: Eq, T> Eq for DataHolder<V, T> {}
-
-impl<V: PartialOrd, T> PartialOrd for DataHolder<V, T> {
-	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-		self.inner.partial_cmp(&other.inner)
-	}
-}
-
-impl<V: Ord, T> Ord for DataHolder<V, T> {
-	fn cmp(&self, other: &Self) -> Ordering {
-		self.inner.cmp(&other.inner)
+impl<V: PartialOrd, T> PartialOrd<V> for DataHolder<V, T> {
+	fn partial_cmp(&self, other: &V) -> Option<Ordering> {
+		self.inner.partial_cmp(other)
 	}
 }
 
 unsafe impl<V: Send, T> Send for DataHolder<V, T> {}
 unsafe impl<V: Sync, T> Sync for DataHolder<V, T> {}
+
+#[cfg(test)]
+mod tests {
+	use super::{DataHolder, Transformer};
+
+	#[derive(Debug, Default, PartialEq)]
+	struct Id(pub u64);
+
+	impl Transformer for Id {
+		type DataType = u64;
+
+		fn transform(&self) -> Self::DataType {
+			self.0
+		}
+
+		fn revert(value: &Self::DataType) -> Self {
+			Self(*value)
+		}
+	}
+
+	#[test]
+	fn persistent_embed() {
+		let value = Id::default();
+
+		let wrapper = DataHolder::from(value);
+
+		assert_eq!(wrapper, 0);
+
+		let value_reverted = wrapper.into_value();
+
+		assert_eq!(value_reverted, Id(0));
+	}
+}
